237fe7730785ad3c57d7d7aea69eb66fe8170a7a
Pasi Niemi <pasi.niemi@nitorcreations.com>
2018-10-01 15:44:24 +0300

    Remove erronous entry point

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 20.16 Pasi Niemi
+# Copyright 20.17 Pasi Niemi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,10 +15,10 @@ import sys
 from setuptools import setup
 
 setup(name='web-auth-ssh',
-      version='0.16',
+      version='0.17',
       description='Tools for authenticating to ssh via keys given out with a web service',
       url='http://github.com/NitorCreations/web-auth-ssh',
-      download_url='https://github.com/NitorCreations/web-auth-ssh/tarball/0.16',
+      download_url='https://github.com/NitorCreations/web-auth-ssh/tarball/0.17',
       author='Pasi Niemi',
       author_email='pasi.niemi@nitor.com',
       license='Apache 2.0',
